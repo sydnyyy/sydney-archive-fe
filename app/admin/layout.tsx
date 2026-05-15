37c09303592aaef7d8d65bbb064de1e25aef3e99
sydnyyy <nayoungko28@gmail.com>
2026-05-16 00:42:34 +0900

    refactor: 어드민 MainNavigation 적용
    - CategorySidebar 제거

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,6 @@
 import "../globals.css";
 import { fontVariables } from "../_shared/fonts";
+import AdminMainNavigation from "@/components/admin/navigation/AdminMainNavigation";
 
 export const dynamic = "force-dynamic";
 
@@ -7,6 +8,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
     return (
         <div className={`${fontVariables} antialiased`}>
             {children}
+            <AdminMainNavigation />
         </div>
     );
 }
---
+++ components/admin/navigation/AdminMainNavigation.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import {
+    HomeIcon,
+    BeakerIcon,
+    ChatBubbleOvalLeftEllipsisIcon,
+    EyeIcon,
+    UserIcon,
+} from "@heroicons/react/24/outline";
+import { useRouter } from "next/navigation";
+
+export default function AdminMainNavigation() {
+
+    const router = useRouter();
+
+    const navItems = [
+        {
+            icon: HomeIcon,
+            action: "/admin/item"
+        },
+        {
+            icon: BeakerIcon,
+            action: "/admin/dev"
+        },
+        {
+            icon: ChatBubbleOvalLeftEllipsisIcon,
+            action: "/admin/chat"
+        },
+        {
+            icon: EyeIcon,
+            action: "/admin/dashboard"
+        },
+        {
+            icon: UserIcon,
+            action: "/admin/login"
+        }
+    ];
+
+    const renderNavButtons = (isMobile = false) =>
+        navItems.map((item, i) => {
+            const Icon = item.icon;
+            const action = item.action;
+
+            return (
+                <button
+                    key={i}
+                    className={`flex flex-col items-center justify-center ${
+                        isMobile ? "flex-11" : "w-full h-16"
+                    }`}
+                    onClick={() => router.push(action)}
+                >
+                    <Icon className="h-6 w-6" />
+                </button>
+            );
+        });
+
+    return (
+        <>
+            {/* 웹: 좌측 */}
+            <nav className="hidden md:flex fixed left-0 top-0 bottom-0 w-20 flex-col items-center pt-[45vh] z-40">
+                <div className="flex flex-col items-center">
+                    {renderNavButtons(false)}
+                </div>
+            </nav>
+
+            {/* 모바일: 하단 */}
+            <nav className="fixed bottom-0 left-0 right-0 z-40 h-14 flex md:hidden">
+                {renderNavButtons(true)}
+            </nav>
+        </>
+    );
+}
