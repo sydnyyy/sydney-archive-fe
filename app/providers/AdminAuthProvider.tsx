bfc22b8d6bd0cbbea0f8e137dca69a2877cd8287
sydnyyy <nayoungko28@gmail.com>
2026-05-29 15:36:29 +0900

    feat: 어드민 전역 인증 Context Provider 구현 및 레이아웃 적용

---
+++ app/providers/AdminAuthProvider.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React, { createContext, useCallback, useContext, useState } from "react";
+import { Admin } from "@/types/domain/user/user";
+import { issueAccessTokenApi } from "@/lib/api/auth/admin.auth.command";
+import { fetchCurrentAdminApi } from "@/lib/api/user/admin.query";
+
+interface AdminAuthContextValue {
+    admin: Admin | null;
+    loading: boolean;
+    accessToken: string | null;
+    loginSync: (sid: string) => Promise<void>;
+}
+
+const AdminAuthContext = createContext<AdminAuthContextValue | null>(null);
+
+export default function AdminAuthProvider({ children }: { children: React.ReactNode }) {
+
+    const [admin, setAdmin] = useState<Admin | null>(null);
+    const [accessToken, setAccessToken] = useState<string | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    const clearAuth = useCallback(() => {
+        setAdmin(null);
+        setAccessToken(null);
+    }, []);
+
+    const loginSync = useCallback(async (sid: string) => {
+        try {
+            setLoading(true);
+
+            const newAccessToken = await issueAccessTokenApi(sid);
+            setAccessToken(newAccessToken);
+
+            const fetchedAdmin = await fetchCurrentAdminApi(newAccessToken);
+            setAdmin(fetchedAdmin);
+
+        } catch (error) {
+            console.error("Auth synchronization failed: ", error);
+            clearAuth();
+        } finally {
+            setLoading(false);
+        }
+    }, [clearAuth]);
+
+    return (
+        <AdminAuthContext.Provider
+            value={{
+                admin,
+                loading,
+                accessToken,
+                loginSync,
+            }}
+        >
+            {children}
+        </AdminAuthContext.Provider>
+    );
+}
+
+export function useAdminAuth() {
+    const ctx = useContext(AdminAuthContext);
+    if (!ctx) {
+        throw new Error("useAuth must be used inside GuestAuthProvider");
+    }
+    return ctx;
+}
