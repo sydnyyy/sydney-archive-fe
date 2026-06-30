7b41f4fba28721ff204e0e1a58b9ea00bf9434c0
sydnyyy <nayoungko28@gmail.com>
2026-06-30 15:04:31 +0900

    refactor: GuestAuthStore 영속화 대상 상태 명시

--- store/useGuestAuthStore.ts
+++ store/useGuestAuthStore.ts
@@ -1,6 +1,5 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-import { GUEST_SID_KEY } from "@/constants/auth/storageKeys";
 
 interface AuthState {
     sid: string | null;
@@ -10,17 +9,22 @@ interface AuthState {
     setHasHydrated: (state: boolean) => void;
 }
 
+export const GUEST_SID_KEY = "sid";
+
 export const useGuestAuthStore = create<AuthState>() (
     persist(
         (set) => ({
             sid: null,
-            setSid: (sid) => set({ sid }),
+            setSid: (sid) => set({ sid: sid }),
             clearSid: () => set({ sid: null }),
             _hasHydrated: false,
             setHasHydrated: (state) => set({ _hasHydrated: state }),
         }),
         {
             name: GUEST_SID_KEY,
+            partialize: (state) => ({
+                sid: state.sid
+            }),
             onRehydrateStorage: () => (state) => {
                 state?.setHasHydrated(true);
             }
